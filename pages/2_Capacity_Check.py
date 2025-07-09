8316c0d5b5d4b4e6218289d3fd3b641dc9233208
Karam Elabd <karam.elabd@gmail.com>
2025-07-09 16:56:46 -0700

    add second page

---
+++ pages/2_Capacity_Check.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Staffing Capacity Check")
+
+st.markdown("""
+This page will help you evaluate whether your current number of QBs and Coaches 
+can support the number of athletes across Levels 3, 4, and 5.
+""")
